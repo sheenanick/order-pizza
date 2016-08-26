c1a842f98f82bf0d33832479520cd2c81cc7e67c
Sheena Nickerson <sheenanick@gmail.com>
2016-08-26 09:35:35 -0700

    add option to choose up to 3 toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-function Pizza(topping, size){
-  this.topping = topping;
+function Pizza(topping1, topping2, topping3, size){
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
   this.size = size;
 }
 
 Pizza.prototype.price = function(){
-  return (this.topping + this.size).toFixed(2);
+  return (this.topping1 + this.topping2 + this.topping3 + this.size).toFixed(2);
 }
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var topping = parseFloat($("#toppings").val());
+    var topping1 = parseFloat($("#toppings").val());
+    var topping2 = parseFloat($("#toppings").val());
+    var topping3 = parseFloat($("#toppings").val());
     var size = parseInt($("input:radio[name=size]:checked").val());
-    var order = new Pizza(topping, size);
+    var order = new Pizza(topping1, topping2, topping3, size);
     $("#price").text(order.price());
     $(".output").show();
   });
